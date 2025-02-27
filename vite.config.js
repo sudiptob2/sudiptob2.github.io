de27c2346381d5f5b825a0fdea0de1f79686bb20
sudipto baral <sudiptobaral.me@gmail.com>
2025-02-26 19:12:16 -0500

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,48 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwind from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-import tailwindConfig from './tailwind.config.mjs';
 import { VitePWA } from 'vite-plugin-pwa';
+import CONFIG from './gitprofile.config';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/<USERNAME>, set base to '/'.
-
-  // If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/, for example your repository is at https://github.com/<USERNAME>/<REPO_NAME>, then set base to '/<REPO_NAME>/'.
-
-  base: '/',
+  base: CONFIG.base || '/',
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        navigateFallback: undefined,
-      },
-      includeAssets: ['logo.png'],
-      manifest: {
-        name: 'Portfolio',
-        short_name: 'Portfolio',
-        description: 'Personal Portfolio',
-        icons: [
-          {
-            src: 'logo.png',
-            sizes: '64x64 32x32 24x24 16x16 192x192 512x512',
-            type: 'image/png',
-          },
-        ],
+    createHtmlPlugin({
+      inject: {
+        data: {
+          metaTitle: CONFIG.seo.title,
+          metaDescription: CONFIG.seo.description,
+          metaImageURL: CONFIG.seo.imageURL,
+        },
       },
     }),
+    ...(CONFIG.enablePWA
+        ? [
+          VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+              navigateFallback: undefined,
+            },
+            includeAssets: ['logo.png'],
+            manifest: {
+              name: 'Portfolio',
+              short_name: 'Portfolio',
+              description: 'Personal Portfolio',
+              icons: [
+                {
+                  src: 'logo.png',
+                  sizes: '64x64 32x32 24x24 16x16 192x192 512x512',
+                  type: 'image/png',
+                },
+              ],
+            },
+          }),
+        ]
+        : []),
   ],
-  css: {
-    postcss: {
-      plugins: [tailwind(tailwindConfig), autoprefixer],
-    },
+  define: {
+    CONFIG: CONFIG,
   },
 });
